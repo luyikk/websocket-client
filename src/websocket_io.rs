c3d33070a62b25684d888edba84467a826cc5ac1
luyikk <luyikk@126.com>
2022-01-28 21:13:14 +0800

    init

---
+++ src/websocket_io.rs
@@ -0,0 +1,234 @@
+use std::cmp::Ordering;
+use std::pin::Pin;
+use std::task::Poll;
+
+use anyhow::{bail, Result};
+use futures_channel::mpsc::Receiver;
+use futures_core::stream::Stream;
+use futures_io::AsyncBufRead;
+use futures_io::AsyncRead;
+use futures_io::AsyncWrite;
+use js_sys::Uint8Array;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::{ErrorEvent, MessageEvent, WebSocket};
+
+pub struct WebsocketIO {
+    pub(crate) ws: WebSocket,
+    pub(crate) reader: WebSocketReader,
+    pub(crate) ws_url: String,
+}
+
+pub struct WebSocketReader {
+    read_rx: Receiver<Uint8Array>,
+    remaining: Vec<u8>,
+}
+pub struct WebSocketWriter {
+    pub(crate) ws: WebSocket,
+    pub(crate) ws_url: String,
+}
+
+unsafe impl Send for WebSocketWriter {}
+unsafe impl Sync for WebSocketWriter {}
+
+impl WebsocketIO {
+    pub async fn new(addr: &str) -> Result<WebsocketIO> {
+        WebsocketIO::new_inner(format!("ws://{}", addr)).await
+    }
+    pub async fn new_wss(addr: &str) -> Result<WebsocketIO> {
+        WebsocketIO::new_inner(format!("wss://{}", addr)).await
+    }
+
+    async fn new_inner(url: String) -> Result<WebsocketIO> {
+        let ws = {
+            match WebSocket::new(&url) {
+                Ok(ws) => ws,
+                Err(err) => bail!("WebSocket new error:{:?}", err),
+            }
+        };
+
+        let buffer = 4;
+        let (open_tx, open_rx) = futures_channel::oneshot::channel();
+        let (read_tx, read_rx) = futures_channel::mpsc::channel(buffer);
+
+        let onmessage_callback = Closure::wrap(Box::new(move |e: MessageEvent| {
+            let mut read_tx = read_tx.clone();
+            let blob = match e.data().dyn_into::<web_sys::Blob>() {
+                Ok(blob) => blob,
+                _ => return,
+            };
+
+            let fr = web_sys::FileReader::new().expect("web_sys::FileReader::new() fail");
+            let fr_c = fr.clone();
+            let file_reader_load_end = Closure::wrap(Box::new(move |_e: web_sys::ProgressEvent| {
+                let array =
+                    Uint8Array::new(&fr_c.result().expect("web_sys::FileReader result() err"));
+                if !read_tx.is_closed() {
+                    if let Err(err) = read_tx.start_send(array) {
+                        log::error!("start_send error: {:?}", err);
+                    }
+                }
+            })
+                as Box<dyn FnMut(web_sys::ProgressEvent)>);
+            fr.set_onloadend(Some(file_reader_load_end.as_ref().unchecked_ref()));
+            file_reader_load_end.forget();
+
+            fr.read_as_array_buffer(&blob).expect("blob not readable");
+        }) as Box<dyn Fn(MessageEvent)>);
+
+        let onerror_callback =
+            Closure::wrap(Box::new(move |_: ErrorEvent| {}) as Box<dyn FnMut(ErrorEvent)>);
+
+        let mut open_tx = Some(open_tx);
+        let onopen_callback =
+            Closure::wrap(Box::new(move |_| open_tx.take().unwrap().send(()).unwrap())
+                as Box<dyn FnMut(JsValue)>);
+
+        ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
+        onmessage_callback.forget();
+
+        ws.set_onerror(Some(onerror_callback.as_ref().unchecked_ref()));
+        onerror_callback.forget();
+
+        ws.set_onopen(Some(onopen_callback.as_ref().unchecked_ref()));
+        onopen_callback.forget();
+
+        let reader = WebSocketReader {
+            read_rx,
+            remaining: Vec::new(),
+        };
+
+        open_rx.await?;
+
+        let ws_io = WebsocketIO {
+            ws,
+            reader,
+            ws_url: url,
+        };
+        Ok(ws_io)
+    }
+
+    pub fn split(self) -> (WebSocketReader, WebSocketWriter) {
+        let WebsocketIO { ws, reader, ws_url } = self;
+        (reader, WebSocketWriter { ws, ws_url })
+    }
+}
+
+impl WebSocketReader {
+    fn write_remaining(&mut self, buf: &mut [u8]) -> usize {
+        match self.remaining.len().cmp(&buf.len()) {
+            Ordering::Less => {
+                let amount = self.remaining.len();
+                buf[0..amount].copy_from_slice(&self.remaining);
+                self.remaining.clear();
+                amount
+            }
+            Ordering::Equal => {
+                buf.copy_from_slice(&self.remaining);
+                self.remaining.clear();
+                buf.len()
+            }
+            Ordering::Greater => {
+                let amount = buf.len();
+                buf.copy_from_slice(&self.remaining[..amount]);
+                self.remaining.drain(0..amount);
+                amount
+            }
+        }
+    }
+}
+
+impl AsyncRead for WebSocketReader {
+    fn poll_read(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &mut [u8],
+    ) -> Poll<std::io::Result<usize>> {
+        if !self.remaining.is_empty() {
+            return Poll::Ready(Ok(self.write_remaining(buf)));
+        }
+
+        let array = match Pin::new(&mut self.read_rx).poll_next(cx) {
+            Poll::Ready(Some(item)) => item,
+            Poll::Ready(None) => return Poll::Pending,
+            Poll::Pending => return Poll::Pending,
+        };
+
+        let array_length = array.length() as usize;
+
+        let read = match array_length.cmp(&buf.len()) {
+            Ordering::Equal => {
+                array.copy_to(buf);
+                buf.len()
+            }
+            Ordering::Less => {
+                array.copy_to(&mut buf[..array_length]);
+                array_length
+            }
+            Ordering::Greater => {
+                self.remaining.resize(array_length, 0);
+                array.copy_to(self.as_mut().remaining.as_mut_slice());
+                self.write_remaining(buf)
+            }
+        };
+
+        Poll::Ready(Ok(read))
+    }
+}
+impl AsyncBufRead for WebSocketReader {
+    fn poll_fill_buf(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<futures_io::Result<&[u8]>> {
+        if !self.remaining.is_empty() {
+            return Poll::Ready(Ok(self.get_mut().remaining.as_slice()));
+        }
+
+        let array = match Pin::new(&mut self.read_rx).poll_next(cx) {
+            Poll::Ready(Some(item)) => item,
+            Poll::Ready(None) => return Poll::Pending,
+            Poll::Pending => return Poll::Pending,
+        };
+
+        self.remaining.extend(&array.to_vec());
+
+        if self.remaining.is_empty() {
+            return Poll::Pending;
+        }
+        Poll::Ready(Ok(self.get_mut().remaining.as_slice()))
+    }
+
+    fn consume(mut self: std::pin::Pin<&mut Self>, amt: usize) {
+        if self.remaining.len() == amt {
+            self.remaining.clear();
+            return;
+        }
+        self.remaining.drain(0..amt);
+    }
+}
+impl AsyncWrite for WebSocketWriter {
+    fn poll_write(
+        self: std::pin::Pin<&mut Self>,
+        _: &mut std::task::Context<'_>,
+        buf: &[u8],
+    ) -> Poll<std::io::Result<usize>> {
+        self.ws.send_with_u8_array(buf).unwrap();
+
+        Poll::Ready(Ok(buf.len()))
+    }
+
+    fn poll_flush(
+        self: std::pin::Pin<&mut Self>,
+        _: &mut std::task::Context<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_close(
+        self: std::pin::Pin<&mut Self>,
+        _: &mut std::task::Context<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        self.ws.close().unwrap();
+        Poll::Ready(Ok(()))
+    }
+}
---
+++ tests/test.rs
@@ -0,0 +1,108 @@
+//! Test suite for the Web and headless browsers.
+#![feature(thread_id_value)]
+#![cfg(target_arch = "wasm32")]
+
+use futures_util::{AsyncBufReadExt, StreamExt};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_test::*;
+use websocket_client::IWebSocketClient;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+wasm_bindgen_test_configure!(run_in_browser);
+
+#[wasm_bindgen_test]
+fn pass() {
+    assert_eq!(1 + 1, 2);
+}
+
+#[wasm_bindgen_test]
+async fn test_websocket() {
+    wasm_logger::init(wasm_logger::Config::default());
+
+    let (tx, rx) = futures_channel::oneshot::channel();
+
+    let ws = websocket_client::WebSocketClient::connect(
+        "127.0.0.1:8888",
+        |_, ws, mut reader| async move {
+            console_log!("connect websocket server ok");
+
+            let mut buf = Vec::new();
+            for _ in 0..1000 {
+                reader.read_until(255, &mut buf).await?;
+                console_log!("{:?}", buf);
+                ws.send_all_ref(&buf).await?;
+                buf.clear();
+            }
+            console_log!("disconnect websocket server");
+            tx.send(()).unwrap();
+            Ok(true)
+        },
+        (),
+    )
+    .await
+    .unwrap();
+
+    for i in 0..=254 {
+        ws.send_all(vec![0, 1, 2, 3,i, 255]).await.unwrap();
+    }
+
+    rx.await.unwrap();
+    console_log!("send ok");
+}
+
+
+#[wasm_bindgen_test]
+async fn test_websocket2() {
+    wasm_logger::init(wasm_logger::Config::default());
+
+    let (mut tx, mut rx) = futures_channel::mpsc::channel(1);
+
+    let ws = websocket_client::WebSocketClient::connect(
+        "127.0.0.1:8888",
+        |_, ws, mut reader| async move {
+            console_log!("connect websocket server ok");
+
+            let mut buf = Vec::new();
+            for i in 0..100 {
+                reader.read_until(255, &mut buf).await?;
+                console_log!("{:?}", buf);
+                ws.send_all_ref(&buf).await?;
+                buf.clear();
+                if !tx.is_closed() {
+                    tx.start_send(i).unwrap()
+                }
+            }
+
+            console_log!("disconnect websocket server");
+
+            if !tx.is_closed() {
+                tx.close_channel();
+            }
+
+            Ok(true)
+        },
+        (),
+    )
+        .await
+        .unwrap();
+
+    ws.send_all(vec![0, 1, 2, 3, 255]).await.unwrap();
+
+    while let Some(i)= rx.next().await {
+        if !ws.is_disconnect() {
+            ws.send_ref(&i32::to_be_bytes(i)).await.unwrap();
+            ws.send_all_ref(&[255]).await.unwrap();
+        }else { break; }
+    }
+
+    console_log!("send ok");
+}
